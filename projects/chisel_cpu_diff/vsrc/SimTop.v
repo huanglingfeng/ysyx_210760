0a297e061359c8375753ba72f90700b30577487f
tracer-oscpu2021 <tracer@oscpu.org>
2021-11-11 19:01:46 +0800

    Huang LingFeng 20210760    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: d2a90473fc5a4e848208fbd6e561064e            Boot ID: 02888d6c403f4c40be67b4dedbe4804a     Virtualization: vmware   Operating System: Ubuntu 20.04 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  19:01:46 up  1:02,  1 user,  load average: 0.45, 0.13, 0.10

--- projects/chisel_cpu_diff/vsrc/SimTop.v
+++ projects/chisel_cpu_diff/vsrc/SimTop.v
@@ -2012,7 +2012,7 @@ module CSR(
       mtime <= _GEN_13;
     end
     if (reset) begin // @[CSR.scala 62:25]
-      mtimecmp <= 64'hff; // @[CSR.scala 62:25]
+      mtimecmp <= 64'hf; // @[CSR.scala 62:25]
     end else if (io_csr_to_lsu_is_clint) begin // @[CSR.scala 72:17]
       if (!(io_csr_to_lsu_load)) begin // @[CSR.scala 73:15]
         if (io_csr_to_lsu_save) begin // @[CSR.scala 75:21]
