c493f168edda07f7a92013931d37c3691528a417
tracer-oscpu2021 <tracer@oscpu.org>
2021-11-11 20:27:53 +0800

    Huang LingFeng 20210760    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: d2a90473fc5a4e848208fbd6e561064e            Boot ID: 02888d6c403f4c40be67b4dedbe4804a     Virtualization: vmware   Operating System: Ubuntu 20.04 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  20:27:53 up  2:28,  1 user,  load average: 0.88, 0.43, 0.27

--- projects/chisel_cpu_diff/src/main/scala/myCPU/CSR.scala
+++ projects/chisel_cpu_diff/src/main/scala/myCPU/CSR.scala
@@ -59,7 +59,7 @@ class CSR extends Module {
   when(true.B) {
     mtime := mtime + 1.U
   }
-  val mtimecmp = RegInit(UInt(64.W),"h3".U)
+  val mtimecmp = RegInit(UInt(64.W),"hbf3".U)
   when(mtime >= mtimecmp){
     mip := Cat(mip(63,8),1.U,mip(6,0)) 
   }
