d91af79aa8950f269712168df7fdaa59ca57d752
20210760-Huang LingFeng <1335841400@qq.com>
2021-11-21 14:26:48 +0800

    通过yield-test，开时钟中断

--- projects/chisel_cpu_diff/src/main/scala/myCPU/CSR.scala
+++ projects/chisel_cpu_diff/src/main/scala/myCPU/CSR.scala
@@ -72,9 +72,9 @@ class CSR extends Module {
   val wdata = io.csr_to_lsu.wdata
 
   val mtime = RegInit(0.U(64.W))
-  // when(true.B) {
-  //   mtime := mtime + 1.U
-  // }
+  when(true.B) {
+    mtime := mtime + 1.U
+  }
   val mtimecmp = RegInit(UInt(64.W),"hf8ff".U)
   when(mtime >= mtimecmp){
     mip := Cat(mip(63,8),1.U,mip(6,0)) 
